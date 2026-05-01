208f52958f864597cd93bb4c772fd61a8f3db661
Delta-Azura <alexis.prev.delta07@gmail.com>
2026-05-01 17:31:57 +0200

    improving info

--- src/main.rs
+++ src/main.rs
@@ -209,13 +209,13 @@ fn info(package: &String) {
         let directory = directory_tmp.to_str().unwrap();
         let file = fs::read_to_string(format!("/var/lib/pkg/DB/{}/META", directory)).unwrap();
         let mut content = file.lines();
-        let name = content.find(|l| l.starts_with('N')).unwrap().to_string();
+        let name = content.find(|l| l.starts_with('N')).unwrap().split_once('N').map(|(_, name)| name).unwrap().to_string();
         println!("Name : {}", name);
-        let version = content.find(|l| l.starts_with('V')).unwrap().to_string();
+        let version = content.find(|l| l.starts_with('V')).unwrap().to_string().split_once('V').map(|(_, version)| version).unwrap().to_string();
         println!("Version = {}", version);
-        let description = content.find(|l| l.starts_with('D')).unwrap().to_string();
+        let description = content.find(|l| l.starts_with('D')).unwrap().to_string().split_once('D').map(|(_, description)| description).unwrap().to_string();
         println!("Description = {}", description);
-        let packager = content.find(|l| l.starts_with('P')).unwrap().to_string();
+        let packager = content.find(|l| l.starts_with('P')).unwrap().to_string().split_once('P').map(|(_, packager)| packager).unwrap().to_string();
         println!("Packager = {}", packager);
 
     } else {
