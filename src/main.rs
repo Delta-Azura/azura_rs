6896916098e1fe7c4e25dd5013ca4e6a7b3f10be
Delta-Azura <alexis.prev.delta07@gmail.com>
2026-05-02 15:51:50 +0200

    progress bar

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,8 @@ use flate2::write::GzEncoder;
 //use walkdir_minimal_copy::{copy_recursive, CopyOptions};
 use std::path::Path;
 use recursive_copy::{copy_recursive, CopyOptions};
+use indicatif::{ProgressBar, ProgressStyle};
+use std::io::Read;
 //use walkdir_minimal_copy::{copy_recursive, CopyOptions};
 
 fn main() {
@@ -218,11 +220,32 @@ fn install(rawpkg: &str) {
 }
 
 fn download(url: &str) -> String {
-    let answer = reqwest::blocking::get(url).unwrap();
-    let bytes = answer.bytes().unwrap();
+    // Personnal notes :
+    // Setting up the first variable to get the answer
+    // Checking lenght of the answer
+    // Setting the progress bar style (random settings)
+    let mut answer = reqwest::blocking::get(url).unwrap();
+    let progress = answer.content_length().unwrap();
+    let pb = ProgressBar::new(progress);
+     pb.set_style(
+        ProgressStyle::default_bar()
+            .template("{msg} [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})")
+            .unwrap()
+            .progress_chars("##"),
+    );
+    // init buf size, 8192 is the most used one 
+    let mut buf = [0u8; 8192];
+    //let bytes = answer.bytes().unwrap();
     let tarball = url.split('/').last().unwrap();
     let mut source = File::create(tarball).unwrap();
-    source.write_all(&bytes).unwrap();
+    // As the downloading goes on we read the answer from the buffer
+    // We write into the file we are downloading and we move the progress bar forward.
+    loop {
+        let n = answer.read(&mut buf).unwrap();
+        if n == 0 { break; }
+        source.write_all(&buf[..n]).unwrap();
+        pb.inc(n as u64);
+    }
     tarball.to_string() //giving back file name
 }
 
