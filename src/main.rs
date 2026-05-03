b58d568f4b2bc30a754609934639c2889a3ba0e8
Delta-Azura <alexis.prev.delta07@gmail.com>
2026-05-03 23:26:40 +0200

    licence

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
-//use ferris_says::say; // from the previous step
+// Raw is a simple package manager written in rust, it aims to be compatible with the Pkgfiles written that works with pkgmk from pkgutils/cards
+//    Copyright (C) 2026  Alexis/Delta-Azura
+
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License along
+//    with this program; if not, write to the Free Software Foundation, Inc.,
+//    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 use std::io::{stdout, BufWriter};
 use std::io;
 use std::env;
@@ -16,7 +32,6 @@ use tar::Builder;
 use xz2::write::XzEncoder;
 use flate2::Compression;
 use flate2::write::GzEncoder;
-//use walkdir_minimal_copy::{copy_recursive, CopyOptions};
 use std::path::Path;
 use recursive_copy::{copy_recursive, CopyOptions};
 use indicatif::{ProgressBar, ProgressStyle};
@@ -24,7 +39,6 @@ use std::io::Read;
 use std::env::current_dir;
 use std::fs::metadata;
 
-//use walkdir_minimal_copy::{copy_recursive, CopyOptions};
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
