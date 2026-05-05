da1be2dcbe614cd651ddb54d4fceb89ca1555ec2
Delta-Azura <alexis.prev.delta07@gmail.com>
2026-05-05 23:35:09 +0200

    recursive dependencies

--- src/depends.rs
+++ src/depends.rs
@@ -1,23 +1,52 @@
 use std::fs;
+use std::collections::HashSet;
+use anyhow::Context;
+use std::path::Path;
 
+pub fn depends(pkg: &str) {//-> Result<(), String> {
+    let mut stack = vec![pkg.to_string()];
+    let mut visited = std::collections::HashSet::new;
 
-
-pub fn depends(pkg: &str) {
-    let path = format!("/var/lib/pkg/DB/{}/META", pkg);
-    let file = fs::read_to_string(format!("/var/lib/pkg/DB/{}/META", pkg)).unwrap();
+    //let path = format!("/var/lib/pkg/DB/{}/META", pkg);
+    //let file = fs::read_to_string(format!("/var/lib/pkg/DB/{}/META", pkg)).unwrap();
     // Adding vect to be able to read properly, without this it would be unable to read if the order isn't respected
    // let mut content: Vec<String> = file.lines().map(|l| l.to_string()).collect();
     //let deps = content.lines().filter(|l| l.starts_with('R'));
-    for i in file.lines().filter(|l| l.starts_with('R')) {
-        if i.is_empty() {
+    while let Some(rawpkg) = stack.pop() {
+        if !visited().insert(rawpkg.clone()) {
+            continue;
+        }
+        //let rawwpkg = format!("{}", rawpkg);
+        if Path::new(&format!("/var/lib/pkg/DB/{}/META", rawpkg)).exists() {
             continue
+        } else {
+            println!("{} isn't installed", rawpkg);
+            std::process::exit(1)
         }
-        if let Some((_, i)) = i.split_once('R') {
-        //let depends = i.split_once('R');
-            let name = i.trim_end_matches(|c: char| c.is_numeric());
-            println!("{}", name);
-            for s in name.lines()
+        let META = std::fs::read_to_string(format!("/var/lib/pkg/DB/{}/META", rawpkg));
+        for i in META.unwrap().lines().filter(|l| l.starts_with('R')) {
+            if i.is_empty() {
+                continue
+            }
+            if let Some((_, deps)) = i.split_once('R') {
+                for dep in deps.lines() {
+                    let name = dep.trim_end_matches(|c: char| c.is_numeric());
+                    stack.push(name.to_string());
+                }
+               // for e in name.lines() {
+                    //if Path::new(format!("/var/lib/pkg/DB/{}", e)).exists() {
+                    //    println!("{} is already installed", e);
+                    //} else {
+                    //    println!("You need to install {}", e);
+                    //    std::process::exit(1)
+                    //}    //env::set_current_dir(format!("/var/lib/pkg/DB/{}", e))
+                //}
+                //println!("{}", name);
+            }
         }
+        println!("{}", rawpkg);
     }
+    //println!("{}", rawpkg);
+    //Ok(())
     
 }
